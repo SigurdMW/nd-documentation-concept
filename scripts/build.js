b3cc81e98d1ad1e40f2d4a4928a143bf0e1ef68b
Sigurd Moland Wahl <sigurd.wahl@bouvet.no>
2020-04-28 20:57:14 +0200

    fixed tasks

--- scripts/build.js
+++ scripts/build.js
@@ -3,6 +3,7 @@ const fs = require("fs").promises
 const makePdf = require("./md-to-pdf")
 const makeHTML = require("./md-to-html")
 const makeJSON = require("./md-to-json")
+const rimraf = require("rimraf")
 require("colors")
 
 const docsPath = path.resolve(__dirname, "../docs")
@@ -11,10 +12,23 @@ const distPath = path.resolve(__dirname, "../dist")
 const allProductMetaPath = path.resolve(distPath, "./products.json")
 const getProductMetaPath = (productName) => path.resolve(__dirname, "../docs/", productName, "meta.json")
 
+const deleteFolder = (path) => new Promise((res, rej) => {
+	rimraf(path, (err) => {
+		if (err) {
+			rej(err)
+			return
+		}
+		res()
+	})
+})
+
 const run = async () => {
 	try {
+		await deleteFolder(distPath)
+		await fs.mkdir(distPath)
 		const allProductDocsDirs = await fs.readdir(docsPath)
 		const { onIteration, onDone } = makeJSON(allProductMetaPath, getProductMetaPath)
+
 		for (const productName of allProductDocsDirs) {
 			await makePdf({ productName, docsPath, distPath: publicPath })
 			await makeHTML({ productName, docsPath, distPath: publicPath })
